a1e101326ff9d08a496e0c2abf53be0f5e2e413f
lrileygraham <lrileygraham@gmail.com>
2017-06-08 14:20:57 -0500

    new

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,13 +34,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['lrileygrahamlongo-bucket'],
-      :access_key_id => ENV['AKIAJV5RYBN5B7ITFJCA'],
-      :secret_access_key=> ENV['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'],
-      :s3_region=> ENV['us-east-1']
-    }
-  }
+
 end
