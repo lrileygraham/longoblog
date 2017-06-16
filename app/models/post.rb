d714700fcb0747832f08979193701f610c39d1af
lrileygraham <lrileygraham@gmail.com>
2017-06-16 16:57:45 -0500

    larger

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
-  has_attached_file :image, styles: {large: "800x800>", medium: "300x300>", thumb: "150x150#"}, default_url: "http://cumberlandsmagic.com/wp-content/themes/vipology_04/images/missing-image-232x150.png"
+  has_attached_file :image, styles: {large: "800x800>", medium: "400x400>", thumb: "150x150#"}, default_url: "http://cumberlandsmagic.com/wp-content/themes/vipology_04/images/missing-image-232x150.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
